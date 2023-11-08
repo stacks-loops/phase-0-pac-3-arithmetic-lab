9d4696b25e2ecd7e14af392fca1e94d82e64a15f
Parker <p.cronin.connolly@gmail.com>
2023-11-08 12:35:30 -0500

    airthmetic lab 15 passing

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+     return a + b;
+    // adds variables a and b together
+  }
+  function subtract(a, b) {
+    return a - b;
+    // we'll fill this in shortly
+  }   
+  function multiply(a, b) {
+    return a * b;
+
+    // we'll fill this in shortly
+  }
+  function divide(a, b) {
+    return a / b;
+    // we'll fill this in shortly
+  }
+  add(a,b);
+  function increment(n) {
+    return ++n;
+    // we'll fill this in shortly
+  }
+  function decrement(n) {
+    return --n;
+    // we'll fill this in shortly
+  }
+  function makeInt(n) {
+    return parseInt(n, 10);
+    // we'll fill this in shortly
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n);
+    // we'll fill this in shortly
+  }
